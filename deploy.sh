cefc68161d22d75949da7e09881d6c413e10de27
Steven Yang <steven.yang@tibco.com>
2020-10-01 17:37:13 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
+docker build -t bigoyang/f1-proxy:latest -t bigoyang/f1-proxy:$SHA ./services/proxy/Dockerfile ./services/proxy
+
+docker push bigoyang/f1-proxy:latest
+docker push bigoyang/f1-proxy:$SHA
+
 kubectl apply -f k8s
+
+kubectl set image deployments/proxy-deployment proxy=bigoyang/f1-proxy:$SHA
